bcb9214b00af362a9d1ec2777f9736232456415e
Rafael Arantes <rafael-fullstack@outlook.com>
2022-12-02 10:01:13 -0300

    Add project files.

---
+++ World.Cup.Simulator/Migrations/20221129123031_AddGroup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace World.Cup.Simulator.Migrations
+{
+    public partial class AddGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Group",
+                table: "Teams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Group",
+                table: "Teams");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Img",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ World.Cup.Simulator/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace World.Cup.Simulator.Models
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public int Group { get; set; }
+    }
+}
